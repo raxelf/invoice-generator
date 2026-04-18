21211feeee9e0c6f690f1a8a7b0255d73f012a6d
raxelf <frananda.rafael@gmail.com>
2026-04-18 22:15:21 +0700

    feat: setup docker compose with zero-setup postgres and go fiber api skeleton

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+
+	"invoice-backend/internal/config"
+	// "invoice-backend/internal/route"
+)
+
+func main() {
+	// local env
+	_ = godotenv.Load()
+
+	config.ConnectDatabase()
+
+	app := fiber.New()
+	
+	// route.RouteSetup(app)
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{"status": "ok"})
+	})
+
+	app.Listen(":8080")
+}
--- server/internal/route/route.go
+++ server/internal/route/route.go
@@ -1,1 +1,14 @@
 package route
+
+import "github.com/gofiber/fiber/v3"
+
+func RouteSetup(app *fiber.App) {
+	// api := app.Group("/api")
+
+	// Public Routes
+	// api.Post("/login", nil)
+	// api.Get("/items", nil)
+
+	// // Protected Routes
+	// api.Post("/invoice", nil)
+}
