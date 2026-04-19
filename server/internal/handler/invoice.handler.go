c0b44274f6577a631a5576a22fbad2c9f7e17cca
raxelf <frananda.rafael@gmail.com>
2026-04-19 13:37:53 +0700

    feat: implement invoice generator with zero trust logic

--- server/internal/handler/invoice.handler.go
+++ server/internal/handler/invoice.handler.go
@@ -1,9 +1,44 @@
 package handler
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"invoice-backend/internal/service"
+	"invoice-backend/internal/utils"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func (h *Handler) CreateInvoice(c fiber.Ctx) error {
-	
+	var input service.CreateInvoiceInput
+
+	if err := c.Bind().Body(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"code":    fiber.StatusBadRequest,
+			"message": "Invalid request body",
+		})
+	}
+
+	// validation
+	if err := utils.ValidateStruct(input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"code":    fiber.StatusBadRequest,
+			"message": err.Error(),
+		})
+	}
+
+	userID, ok := c.Locals("user_id").(string)
+	if !ok || userID == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"code":    fiber.StatusUnauthorized,
+			"message": "Unauthorized",
+		})
+	}
+
+	if err := service.CreateInvoice(h.DB, input, userID); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"code": fiber.StatusInternalServerError,
+			"message": err.Error(),
+		})
+	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"code":    fiber.StatusOK,
